62653e3e077b0eba02b5f936e52579e7373a0969
FilipUrbanek406 <filipurbanek406@gmail.com>
2025-03-07 15:24:11 +0100

    Vytvoření tříd, základních metod a řazení portfólia

--- src/main/java/com/engeto/crypto/CryptoController.java
+++ src/main/java/com/engeto/crypto/CryptoController.java
@@ -1,4 +1,29 @@
 package com.engeto.crypto;
 
+import java.util.*;
+
 public class CryptoController {
+
+    private List<Crypto> cryptos = new ArrayList<>();
+
+    public void addCrypto(Crypto crypto) { cryptos.add(crypto); }
+
+    public List<Crypto> getCryptos(){
+        return cryptos;
+    }
+
+    public List<Crypto> sortCryptosByName() {
+        cryptos.sort(Comparator.comparing(Crypto::getName));
+        return cryptos;
+    }
+
+    public List<Crypto> sortCryptosByPrice() {
+        cryptos.sort(Comparator.comparing(Crypto::getPrice));
+        return cryptos;
+    }
+
+    public List<Crypto> sortCryptosByQuantity() {
+        cryptos.sort(Comparator.comparing(Crypto::getQuantity));
+        return cryptos;
+    }
 }
